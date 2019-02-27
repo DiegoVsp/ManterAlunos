5c6f28a83f230b778b7a67cf258a2c223083a538
Diego Vespa <diego-vespa2020@outlook.com.br>
2019-02-26 22:36:46 -0300

    ExercicioAva

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { informacoes } from '../Info/Informacoes';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+
+  public informacoes: informacoes[];
+
+  constructor(public http:HttpClient){
+
+  }
+
+  ngOnInit(): void{
+    this.http.get<informacoes[]>('https://gilsonpolito-api.herokuapp.com/alunos')
+    .subscribe(
+      (Informacoes)=>{
+        this.informacoes = this.informacoes;
+      }
+    )
+  }
 
 }
